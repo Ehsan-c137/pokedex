3a29712a4812cfaab03e3d8ea3c635c0dea798e9
Ehsan <ehsanmousavi1.79@gmail.com>
2025-09-07 11:36:14 +0330

    feat: add home link

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -3,9 +3,16 @@ import { Link, Outlet } from "react-router-dom"
 export function MainLayout() {
    return (
       <div className="container mx-auto min-h-screen">
-         <header className="px-2 py-1 flex justify-center items-center h-20 bg-red-400">
+         <header className="flex gap-4 items-center px-2 py-1 justify-center  h-20 bg-red-400">
             <Link to="/">
-               <p className="font-bold text-white text-2xl">Pokédex</p>
+               <p className="font-bold text-white text-2xl hover:text-red-600 transition">
+                  Pokédex
+               </p>
+            </Link>
+            <Link to={"/"}>
+               <p className="font-bold text-white text-2xl hover:text-blue-400 transition">
+                  Home
+               </p>
             </Link>
          </header>
          <main
